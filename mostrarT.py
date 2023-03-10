497b455539bf4f0987b2fd4e9c37d62a4060d931
Micael Santos <micaelds243@gmail.com>
2023-03-10 17:58:50 -0300

    conexão de um arquivo python com outro arquivo python, e loops

---
+++ mostrarT.py
@@ -0,0 +1,5 @@
+import tabuada
+
+testeF = tabuada.teste
+
+print(testeF)
---
+++ tabuada.py
@@ -0,0 +1,12 @@
+# n = int(input('Digite um número'))
+
+
+l = [1,2,3,4,5,6,7,8,9,10]
+
+for conta in l: 
+     print(n * conta);
+
+("=" * 20)     
+
+for i in range(0,10,3):
+    print(i)
