bdc7debc5a20224d54a0678837830a2a1d17416c
Micael Santos <micaelds243@gmail.com>
2023-03-10 18:59:44 -0300

    Versão final

--- mostrarT.py
+++ mostrarT.py
@@ -1,5 +1,11 @@
 import tabuada
 
-testeF = tabuada.teste
+r = tabuada.n
 
-print(testeF)
+
+l = [1,2,3,4,5,6,7,8,9,10]
+
+for conta in l: 
+     print('{} x {} = {}'.format(r,conta,r * conta) );
+
+("=" * 20) 
--- tabuada.py
+++ tabuada.py
@@ -1,12 +1,1 @@
-# n = int(input('Digite um número'))
-
-
-l = [1,2,3,4,5,6,7,8,9,10]
-
-for conta in l: 
-     print(n * conta);
-
-("=" * 20)     
-
-for i in range(0,10,3):
-    print(i)
+n = int(input('Digite um número: '))
